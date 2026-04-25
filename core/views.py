efb7d910888f5d1f37000ccf6c750542868b01cd
Dino Tan <dinotan0730@gmail.com>
2026-04-25 15:33:15 +0800

    add room

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .model import Room
 
 def home(request):
@@ -14,3 +14,35 @@ def dashboard(request):
 def room_list(request):
     rooms = Room.objects.all()
     return render(request, 'room_list.html', {'rooms': rooms})
+
+
+def add_room(request):
+    if request.method == 'POST':
+        roomnumber = request.POST.get('roomnumber')
+        roomtype = request.POST.get('roomtype')
+        size = request.POST.get('size')
+        price = request.POST.get('price')
+
+        if request.POST.get('availability') :
+            availability = True
+        else:
+            availability = False
+            
+        description = request.POST.get('description')
+
+
+        Room.objects.create(
+            roomnumber = roomnumber,  #fieldname(from model.py) = variable 
+            roomtype = roomtype,
+            size = size,
+            price = price,
+            availability = availability,
+            description = description
+        )
+
+        return redirect('room_list')
+    
+    else:
+         return render(request, 'add_room.html')
+        
+    
