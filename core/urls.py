db1fb25c54ab37fedb5720bf26bad2c0dbc6cf70
Simon Paull <simonpaull439@email.com>
2026-05-03 19:01:45 +0800

    Fixed and completed change password functionality

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,61 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register_view, name='register'),
+
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('tenant-dashboard/', views.tenant_dashboard, name='tenant_dashboard'),
     path('profile/', views.profile_view, name='profile'),
     path('edit-profile/', views.edit_profile, name='edit_profile'),
-    path('register/', views.register_view, name='register'),
-]
 
+    path(
+        'change-password/',
+        auth_views.PasswordChangeView.as_view(
+            template_name='change_password.html',
+            success_url='/change-password-done/'
+        ),
+        name='change_password'
+    ),
+    path(
+        'change-password-done/',
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name='change_password_done.html'
+        ),
+        name='password_change_done'
+    ),
+
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(
+            template_name='password_reset.html'
+        ),
+        name='password_reset'
+    ),
+    path(
+        'password-reset-done/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='password_reset_done.html'
+        ),
+        name='password_reset_done'
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='password_reset_confirm.html'
+        ),
+        name='password_reset_confirm'
+    ),
+    path(
+        'reset-done/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='password_reset_complete.html'
+        ),
+        name='password_reset_complete'
+    ),
+
+]
--- rental_system/settings.py
+++ rental_system/settings.py
@@ -117,3 +117,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email settings for password reset
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'simonpaull439@gmail.com'
+EMAIL_HOST_PASSWORD = 'mxdm eakg xbjx myjd'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
