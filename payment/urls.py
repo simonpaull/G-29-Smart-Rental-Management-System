29a69e7d4f3b1f17cf0dd6c79f823ec05cbdf825
Shyan <yyishyan8@gmail.com>
2026-05-11 14:48:15 +0800

    added PDF receipt, email with attachment, download receipt

--- payment/urls.py
+++ payment/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.tenant_payment_dashboard, name='tenant_payment_dashboard'),
     path('<int:payment_id>/', views.payment_page, name='payment_page'),
+    path('<int:payment_id>/receipt/', views.download_receipt, name='download_receipt'),
     path('history/', views.payment_history, name='payment_history'),
     path('admin-history/', views.admin_payment_history, name='admin_payment_history'),
     path('success/', views.payment_success, name='payment_success'),
--- rental_system/settings.py
+++ rental_system/settings.py
@@ -117,3 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'srm.adminapi@gmail.com'
+EMAIL_HOST_PASSWORD = 'eykt ckxi lgfc kyfc'
+DEFAULT_FROM_EMAIL = 'srmadminapi@gmail.com'
